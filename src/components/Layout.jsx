ccd633de473ae3e21d1528f47e8353dc864d7874
Hemeline FT <emeline.farget@gmail.com>
2024-03-03 17:50:32 +0100

    Suppression de code inutile sur le composant Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -15,28 +15,4 @@ function Layout() {
   )
 }
 
-// function Dashboard() {
-//     return (
-//       <div>
-//         <h1>Chez vous, partout et ailleurs</h1>
-  
-//         {/* This element will render either <DashboardMessages> when the URL is
-//             "/messages", <DashboardTasks> at "/tasks", or null if it is "/"
-//         */}
-//         <Layout />
-//       </div>
-//     );
-//   }
-  
-//   function App() {
-//     return (
-//       <Routes>
-//         <Route path="/" element={<Dashboard />}>
-//           <Route path="messages" element={<DashboardMessages />} />
-//           <Route path="tasks" element={<DashboardTasks />} />
-//         </Route>
-//       </Routes>
-//     );
-//   }
-
   export default Layout;
