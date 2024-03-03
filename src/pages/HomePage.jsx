c054912c0919ba22e006539800e35f3d11924864
Hemeline FT <emeline.farget@gmail.com>
2024-03-03 18:10:40 +0100

    Suppression de code inutile

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,33 +1,3 @@
-// import React from 'react';
-// import style from './HomePage.module.scss';
-// import Logo from '../images/logoKasa.png';
-// import Cards from '../components/Cards';
-// import data from '../data/logements.json';
-
-// function HomePage() {
-//         const Cards = ({ data }) => {
-//             return (
-//               <div>
-//                   <img src={Logo} alt="Kasa, location d'appartements" />
-//                   <p>Chez vous, partout et ailleurs</p>
-//                 <ul>
-//                 {data && data.map((item, index) => (
-//                       <li key={index}>{item}</li>
-//                 ))}
-//                 </ul>
-//               </div>
-//             );
-//         };
-
-//     return (
-//         <div>
-//             <Cards data={['Donnée 1', 'Donnée 2', 'Donnée 3']} />
-//         </div>
-//     );
-// }
-
-// export default HomePage;
-
 import React from 'react';
 import style from './HomePage.module.scss';
 import Cards from '../components/Cards'; // Import du composant Cards
