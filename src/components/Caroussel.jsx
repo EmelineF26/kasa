9c39ac23a9f4d0f411629ed66638db118a9ad2f5
Hemeline FT <emeline.farget@gmail.com>
2024-05-09 18:29:51 +0200

    Conversion CSS en SCSS + supression de code inutile + Réalisation du composant Caroussel

--- src/components/Caroussel.jsx
+++ src/components/Caroussel.jsx
@@ -4,11 +4,23 @@ import { useState } from 'react';
 function Caroussel({ pictures, title }) {
     const [index, setIndex] = useState(0);
 
+    const nextPicture = () => {
+        const newIndex = (index + 1) % pictures.length;
+        setIndex(newIndex);
+    };
+
+    const prevPicture = () => {
+        const newIndex = (index - 1 + pictures.length) % pictures.length;
+        setIndex(newIndex);
+    };
+
     return (
         <div>
+            <button onClick={prevPicture}>Previous</button>
             <img src={pictures[index]} alt={title} />
+            <button onClick={nextPicture}>Next</button>
         </div>
-    )
+    );
 }
 
 export default Caroussel;
